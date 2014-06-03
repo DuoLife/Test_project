e24c977a201223cf4b04fd036c2cb1dc73bcddbe
xuming <vip6ming@126.com>
2014-06-03 21:10:55 +0800

    socket

--- src/socket/TestSocket.java
+++ src/socket/TestSocket.java
@@ -1,5 +1,26 @@
 package socket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class TestSocket {
 
+	public static void main(String[] args) {
+		InputStreamReader input = new InputStreamReader(System.in);
+		OutputStream output = new 
+//		Scanner s = new Scanner(System.in);
+//		System.out.println(s.next() + "console");
+		System.out.println(input.);
+//		try {
+//			Socket client = new Socket("127.0.0.1", 12345);
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+	}
 }
