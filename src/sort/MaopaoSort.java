3368add8dd5dc155aa19fa79a4be987ec3e740dd
xuming <vip6ming@126.com>
2014-07-11 20:48:17 +0800

    qq飞车修改; js数据转换习题完成

--- src/sort/MaopaoSort.java
+++ src/sort/MaopaoSort.java
@@ -17,7 +17,7 @@ public class MaopaoSort {
 		int size = array.length;
 		int temp;
 		for(int i = 0; i < (size-1); i++) {
-			for(int j = 0; j < (size-i); j++) {
+			for(int j = 0; j < (size-i-1); j++) {
 				if(array[j]>array[j+1]) {
 					temp = array[j];
 					array[j] = array[j+1];
--- src/sort/SortUtil.java
+++ src/sort/SortUtil.java
@@ -3,41 +3,41 @@ package sort;
 public class SortUtil {
 
 	public static void quickSort(int a[], int start, int end) {
-		int i,j;
-		i=start;
-		j=end;
-		if((a==null)||(a.length==0))
-		return;
-		while(i<j){
-		while(i<j&&a[i]<=a[j]){//以数组start下标的数据为key，右侧扫描
-		j--;
-		}
-		if(i<j){//右侧扫描，找出第一个比key小的，交换位置
-		int temp=a[i];
-		a[i]=a[j];
-		a[j]=temp;
-		}
-		while(i<j&&a[i]<a[j]){//左侧扫描（此时a[j]中存储着key值）
-		i++;
-		}
-		if(i<j){//找出第一个比key大的，交换位置
-		int temp=a[i];
-		a[i]=a[j];
-		a[j]=temp;
-		}
-		if(i-start>1){
-		//递归调用，把key前面的完成排序
-		quickSort(a,start,i-1);
-		}
-		if(end-i>1){
-		quickSort(a,i+1,end);//递归调用，把key后面的完成排序
-		}
+		int i, j, len, temp;
+		i = start;
+		j = end;
+		len = a.length;
+		while( i<j ) {
+			while( i<j && a[i] <= a[j]) {
+				j--;
+			}
+			if( i<j) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+			}
+			while( i<j && a[i] < a[j]) {
+				i++;
+			}
+			if( i<j) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				j--;
+			}
+			if( i-start>1) {
+				quickSort( a, start, i-1);
+			}
+			if( end-i>1) {
+				quickSort( a, i+1, end);
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
-		//int[] a = {59, 1, 22, 45, 21, 65, 33, 76};
-		int[] a = {4,3,2,1,0};
+		int[] a = {59, 1, 22, 45, 21, 65, 33, 76};
+		//int[] a = {4,3,2,1,0};
 		quickSort(a, 0, a.length-1);
 		for(int i: a){
 			System.out.print(i+" ");
